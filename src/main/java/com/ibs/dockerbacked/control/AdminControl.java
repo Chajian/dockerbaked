923ca9fd5c176e2ca0482924b145b3a92a91266b
Chajian <936796603@qq.com>
2022-08-18 16:15:33 +0800

    add admin api struction

--- src/main/java/com/ibs/dockerbacked/control/AdminControl.java
+++ src/main/java/com/ibs/dockerbacked/control/AdminControl.java
@@ -1,9 +1,17 @@
 package com.ibs.dockerbacked.control;
 
+import com.ibs.dockerbacked.data.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("ibs/api/admin")
 public class AdminControl {
+
+    @GetMapping(value = "containers")
+    public ResponseBody getContainers(){
+        return null;
+    }
+
 }
