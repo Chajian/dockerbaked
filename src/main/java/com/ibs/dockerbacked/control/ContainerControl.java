3c01f2156c447fe5cee2ae6f7c0d44cf9afeb7fa
Chajian <936796603@qq.com>
2022-08-18 16:14:13 +0800

    init api struction

--- src/main/java/com/ibs/dockerbacked/control/ContainerControl.java
+++ src/main/java/com/ibs/dockerbacked/control/ContainerControl.java
@@ -1,12 +1,49 @@
 package com.ibs.dockerbacked.control;
 
+import com.ibs.dockerbacked.data.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("ibs/api/containers")
 public class ContainerControl {
 
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public ResponseBody getContainers(boolean all,String filters){
+        return null;
+    }
+
+    @RequestMapping(value = "/create",method = RequestMethod.POST)
+    public ResponseBody createContainer(){
+        return null;
+    }
+
+    @RequestMapping(value = "/{containerId}",method = RequestMethod.GET)
+    public ResponseBody deleteContainer(@PathVariable String id){
+        return null;
+    }
+    @RequestMapping(value = "/{containerId}/start",method = RequestMethod.POST)
+    public ResponseBody startContainer(@PathVariable String id){
+        return null;
+    }
+    @RequestMapping(value = "/{containerId}/stop",method = RequestMethod.POST)
+    public ResponseBody stopContainer(@PathVariable String id){
+        return null;
+    }
+    @RequestMapping(value = "/{containerId}/restart",method = RequestMethod.POST)
+    public ResponseBody restartContainer(@PathVariable String id){
+        return null;
+    }
+    @RequestMapping(value = "/{containerId}/pause",method = RequestMethod.POST)
+    public ResponseBody pauseContainer(@PathVariable String id){
+        return null;
+    }
+    @RequestMapping(value = "/{containerId}/rename",method = RequestMethod.POST)
+    public ResponseBody renameContainer(@PathVariable String id){
+        return null;
+    }
 
 
 }
--- src/main/java/com/ibs/dockerbacked/control/ImageControl.java
+++ src/main/java/com/ibs/dockerbacked/control/ImageControl.java
@@ -1,9 +1,24 @@
 package com.ibs.dockerbacked.control;
 
+import com.ibs.dockerbacked.data.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("ibs/api/images")
 public class ImageControl {
+
+    @GetMapping(value = "")
+    public ResponseBody getImages(){
+        return null;
+    }
+
+    @PostMapping(value = "pull")
+    public ResponseBody pullImage(){
+        return null;
+    }
+
+
 }
