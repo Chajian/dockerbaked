f7d70220ef1dd650098a2cb1f88ef66175446d33
asdzxc1 <1240757402@qq.com>
2023-02-28 22:56:11 +0800

    delete some UnityTest

--- src/test/java/com/ibs/dockerbacked/mapper/ImageMapperTest.java
+++
@@ -1,9 +0,0 @@
-package com.ibs.dockerbacked.mapper;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-@Slf4j
-public class ImageMapperTest {
-}
