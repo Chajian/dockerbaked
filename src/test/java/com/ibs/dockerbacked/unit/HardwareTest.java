ddeb6ed7eeb96e7650c6a0447cf6d67997884b82
asdzxc1 <1240757402@qq.com>
2023-03-01 14:42:13 +0800

    Hardware test success

---
+++ src/test/java/com/ibs/dockerbacked/unit/HardwareTest.java
@@ -0,0 +1,46 @@
+package com.ibs.dockerbacked.unit;
+
+import com.ibs.dockerbacked.entity.Hardware;
+import com.ibs.dockerbacked.service.HardwareService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class HardwareTest {
+    @Autowired
+    private HardwareService hardwareService;
+
+    @Test
+    public void HardwareAll() {
+        System.out.println(hardwareService.getById(2));
+    }
+
+    @Test
+    public void HardwareInsert() {
+        Hardware hardware = new Hardware();
+        hardware.setCpuType("1");
+        hardware.setCpuCoreNumber(1);
+        hardware.setDisk(1);
+        hardware.setMemory(100);
+        hardware.setNetworkSpeed(12);
+        hardwareService.save(hardware);
+    }
+
+    @Test
+    public void HardwareUpdate() {
+        Hardware hardware = new Hardware();
+        hardware.setId(2);
+        hardware.setCpuType("1");
+        hardware.setCpuCoreNumber(2);
+        hardwareService.saveOrUpdate(hardware);
+    }
+
+    @Test
+    public void HardwareRemove() {
+        hardwareService.removeById(3);
+    }
+}
