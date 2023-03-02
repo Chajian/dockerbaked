5979c4fae35670d43be4c2994d8e1baea55c103f
asdzxc1 <1240757402@qq.com>
2023-03-02 20:15:26 +0800

    asdfc

--- src/main/java/com/ibs/dockerbacked/entity/User.java
+++ src/main/java/com/ibs/dockerbacked/entity/User.java
@@ -14,14 +14,10 @@ import java.util.List;
  */
 
 @TableName("users")
-<<<<<<< HEAD
 @Data
+
 public class User extends TimeRecord {
-    private int id;
-=======
-public class User extends TimeRecord{
     private Integer id;
->>>>>>> 4aba4c5c63cc975568f6c1a10ed7576a9a7b6bb9
     private String account;
     private String pwd;
 }
