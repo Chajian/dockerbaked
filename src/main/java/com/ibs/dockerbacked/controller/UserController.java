cc7b99808726c675daf24d8f360afb04f667de26
asdzxc1 <1240757402@qq.com>
2023-03-02 20:30:41 +0800

    asdas

---
+++ src/main/java/com/ibs/dockerbacked/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.ibs.dockerbacked.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author sn
+ * @version 1.0
+ * @descript 用户接口
+ * @date 2023/3/2
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+}
--- src/main/java/com/ibs/dockerbacked/service/ContainerService.java
+++ src/main/java/com/ibs/dockerbacked/service/ContainerService.java
@@ -3,16 +3,14 @@ package com.ibs.dockerbacked.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.ibs.dockerbacked.entity.Container;
 
-<<<<<<< HEAD
+
 
 /**
  * @author sn
  */
-=======
 import java.util.List;
 import java.util.Map;
 
->>>>>>> 4aba4c5c63cc975568f6c1a10ed7576a9a7b6bb9
 public interface ContainerService extends IService<Container> {
     //获取容器
     List<Container> getContainersByStatus(Integer containerId, List<String> status);
