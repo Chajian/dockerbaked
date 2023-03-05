e8ad433695775fd2d39536bc6b9b215cdcf8e586
trusted <936796603@qq.com>
2023-03-05 20:29:33 +0800

    add test about container

---
+++ src/test/java/com/ibs/dockerbacked/connection/TestContainer.java
@@ -0,0 +1,24 @@
+package com.ibs.dockerbacked.connection;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestContainer {
+    private DockerConnection dockerConnection;
+
+    private ContainerModel containerModel;
+
+    @Before
+    public void init(){
+        dockerConnection = new DockerConnection("dockerxylyjy","docker@123789","xylyjy@gmail.com","npipe:////./pipe/docker_engine","https://index.docker.io/v1/");
+        containerModel = new ContainerModel(dockerConnection.connect());
+    }
+
+
+    @Test
+    public void getInfo(){
+        containerModel.inspectContainer("");
+    }
+
+
+}
