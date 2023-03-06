6e9e014942d8463ed31201846b15259d4240907e
trusted <936796603@qq.com>
2023-03-06 17:53:41 +0800

    add docker config and container test

--- src/test/java/com/ibs/dockerbacked/connection/TestContainer.java
+++ src/test/java/com/ibs/dockerbacked/connection/TestContainer.java
@@ -1,5 +1,6 @@
 package com.ibs.dockerbacked.connection;
 
+import com.ibs.dockerbacked.util.EntityUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,14 +11,19 @@ public class TestContainer {
 
     @Before
     public void init(){
-        dockerConnection = new DockerConnection("dockerxylyjy","docker@123789","xylyjy@gmail.com","npipe:////./pipe/docker_engine","https://index.docker.io/v1/");
+        dockerConnection = new DockerConnection("","","","unix:///var/run/docker.sock","https://index.docker.io/v1/");
         containerModel = new ContainerModel(dockerConnection.connect());
     }
 
 
     @Test
     public void getInfo(){
-        containerModel.inspectContainer("");
+        EntityUtils.print(containerModel.inspectContainer("d24447f0f10b"));
+    }
+
+    @Test
+    public void stop(){
+        containerModel.stopContainer("d24447f0f10b");
     }
 
 
