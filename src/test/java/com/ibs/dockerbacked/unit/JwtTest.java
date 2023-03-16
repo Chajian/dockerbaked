f55ee425a285c87a651c8367fca17a63ab790880
trusted <936796603@qq.com>
2023-03-16 14:37:41 +0800

    add the parameter of id at verify and sign methods int Jwtutils

--- src/test/java/com/ibs/dockerbacked/unit/JwtTest.java
+++ src/test/java/com/ibs/dockerbacked/unit/JwtTest.java
@@ -1,4 +1,29 @@
 package com.ibs.dockerbacked.unit;
 
+import com.ibs.dockerbacked.entity.User;
+import com.ibs.dockerbacked.mapper.UserMapper;
+import com.ibs.dockerbacked.util.JwtUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class JwtTest {
+    @Autowired
+    UserMapper userMapper;
+
+
+    @Test
+    public void test(){
+        User user = userMapper.selectById(1);
+        String token = JwtUtil.sign(user.getAccount(),user.getId());
+        if(JwtUtil.verity(token,user)){
+            System.out.println("验证成功!"+"账号:"+JwtUtil.getUserAccount(token)+"id:"+JwtUtil.getUserId(token));
+        }
+    }
+
+
 }
