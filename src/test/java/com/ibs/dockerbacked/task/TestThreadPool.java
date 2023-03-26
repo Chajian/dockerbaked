3efecf5d3ec5eaacd13ba8faf42c6bddd5b2e6bb
trusted <936796603@qq.com>
2023-03-27 00:02:39 +0800

    test ThreadPool

--- src/test/java/com/ibs/dockerbacked/task/TestThreadPool.java
+++ src/test/java/com/ibs/dockerbacked/task/TestThreadPool.java
@@ -7,27 +7,25 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-/**
- * 线程测试
- * @author Yanglin
- */
-public class TestTaskThread {
-    TaskThread taskThread;
-    List<TaskThread> tasks = new ArrayList<>();
-    int current = 0;
-    List<Thread> list = new ArrayList<>();
+public class TestThreadPool {
+    ExecutorService executor;
     int max = 10000;
+    List<TaskThread> tasks = new ArrayList<>();
+
 
     @Before
     public void init(){
-        tasks.add(new TaskThread(max));
+        executor = Executors.newFixedThreadPool(10);//线程池
+
     }
 
-
     @Test
-    public void addTask(){
-        for(int i = 0 ; i < 1000000 ; i++){
+    public void addThread(){
+        int current = -1;
+        for(int i = 0 ; i < max*10 ; i++){
             BaseTask baseTask = new BaseTask(i) {
                 @Override
                 public void start() {
@@ -46,23 +44,21 @@ public class TestTaskThread {
                 }
             };
 
-            if(!tasks.get(current).add(baseTask)){
+            if(tasks.size()<=0||!tasks.get(current).add(baseTask)){
                 current++;
                 tasks.add(new TaskThread(max));
             }
         }
         run();
-
     }
+
 
     public void run(){
         for(int i = 0 ; i < tasks.size() ;i++){
-            Thread t = new Thread(tasks.get(i));
-            t.start();
-            list.add(t);
+            executor.execute(tasks.get(i));
         }
-        System.out.println("ss");
         while(true);
     }
+
 
 }
