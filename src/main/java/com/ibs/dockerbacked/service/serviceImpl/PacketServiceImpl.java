8483f9a6599555585a51bbdf60325b999bcb7ce8
asdzxc1 <[email]>
2023-05-12 16:41:41 +0800

    alter UserImpl and PackerImpl

--- src/main/java/com/ibs/dockerbacked/service/serviceImpl/PacketServiceImpl.java
+++ src/main/java/com/ibs/dockerbacked/service/serviceImpl/PacketServiceImpl.java
@@ -19,7 +19,9 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -31,7 +33,8 @@ public class PacketServiceImpl extends ServiceImpl<PacketMapper, Packet> impleme
     @Autowired
     @Lazy
     private HardwareService hardwareService;
-
+    @Autowired
+    private PacketMapper packetMapper;
     @Lazy
     @Autowired
     private PacketService currentPorxy;
@@ -44,52 +47,66 @@ public class PacketServiceImpl extends ServiceImpl<PacketMapper, Packet> impleme
         if (isFree) {
             //cas操作保证金钱操作正确
             AtomicFloat atomicFloat = new AtomicFloat(0l);
+            List<Float> count = new ArrayList<>();
             //配置cpu核数的收费规则
             float cpuCharge = hardware.getCpuCoreNumber() * hardware.getCpuCoreNumberMoney();
+            count.add(cpuCharge);
             //网数收费
             float netWorkSpeedCharge = hardware.getNetworkSpeed() * hardware.getNetworkSpeedMoney();
+            count.add(netWorkSpeedCharge);
             //cpu类型的收费
             float cpuTypeCharge = Integer.parseInt(hardware.getCpuType()) * hardware.getCpuTypemoney();
+            count.add(cpuTypeCharge);
             //磁盘收费
             float diskCharge = hardware.getDisk() * hardware.getDiskMoney();
-
+            count.add(diskCharge);
+            //内存收费
+            float memoryCharge = hardware.getMemory() * hardware.getMemory();
+            count.add(memoryCharge);
             //原子操作
-            float memoryCount = atomicFloat.addAndGet
-                    (cpuCharge + netWorkSpeedCharge + cpuTypeCharge + diskCharge);
+            count.forEach(money -> {
+                AtomicFloat atomic = new AtomicFloat(0l);
+                atomicFloat.addAndGet(atomic.addAndGet(money));
+            });
 
-            //金额不一致抛出异常
-            if (memoryCount != hardware.getMemory()) throw new CustomExpection(500, "金额不正确");
-
-            boolean isSuccess = currentPorxy.addHardwarePacketDP(hardware, memoryCount);
-            if (!isSuccess) throw new CustomExpection(500,"购买失败");
+            //套餐资源
+            boolean isSuccess = currentPorxy.addHardwarePacketDP(hardware, isFree, atomicFloat.get());
+            if (!isSuccess) throw new CustomExpection(500, "购买失败");
             return Result.success(200, "购买成功", hardware);
         }
 
         //非弹性设置默认
+        //查询硬件
         HardwareDto hardwareDto = new HardwareDto();
         Hardware hard = hardwareService.getById(3);
         if (hard == null) throw new CustomExpection(500, "error");
-        Packet packet = getById(5);
-        if (packet == null) throw new CustomExpection(500, "error");
         BeanUtils.copyProperties(hard, hardwareDto);
-        BeanUtils.copyProperties(packet, hardwareDto);
+        //资源
+        boolean isSuccess = currentPorxy.addHardwarePacketDP(hardware, isFree, hard.getMoney());
+        if (!isSuccess) throw new CustomExpection(500, "购买失败");
         return Result.success(200, "success", hardwareDto);
     }
 
     @Transactional
-    public boolean addHardwarePacketDP(HardwareDto hardware, float memoryCount) {
-        if (memoryCount != hardware.getMemory()) throw new CustomExpection(500, "金额不正确");
+    public boolean addHardwarePacketDP(HardwareDto hardware, boolean isFree, float money) {
         //硬件资源
         Hardware hardwareDB = new Hardware();
-        BeanUtils.copyProperties(hardware, hardwareDB);
+        BeanUtils.copyProperties(hardware, hardwareDB, "id");
         hardwareDB.setCreatedAt(new Date());
-        hardwareDB.setMemory(memoryCount);
-        hardwareService.save(hardwareDB);
+        hardwareDB.setMoney(money);
+        packetMapper.addGetId(hardwareDB);
         //创建套餐
         Packet packet = new Packet();
+        if (isFree) {
+            packet.setDescription("1"); //设置详情信息
+            packet.setName("222"); //设置套餐名
+        } else {
+            //查询套餐
+            Packet packetDefalut = getById(4);
+            if (packetDefalut == null) throw new CustomExpection(500, "error");
+            BeanUtils.copyProperties(packetDefalut,packet,"id");
+        }
         packet.setHardwareId(hardwareDB.getId()); //设置硬件id
-        packet.setDescription("1"); //设置详情信息
-        packet.setName("222"); //设置套餐名
         packet.setCreatedAt(new Date());
         save(packet);
         return true;
